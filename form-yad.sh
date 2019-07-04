e360ec716cd006bd4718f71a474adbf76123467d
Roque Souza <rlaecio@hotmail.com>
2019-07-04 10:21:05 +0100

    yad/zenity

--- form-yad.sh
+++ form-yad.sh
@@ -1,6 +1,6 @@
 #/bin/bash
 #Exemplo yad
-Saida=$(yad --form --imag blog-min.png --image-on-top \ 
+Saida=$(zenity --form --imag blog-min.png --image-on-top \ 
     --title "Terminal Root" \
     --text "Exemplo <b>YAD</b> por <big><b>Terminal Roo</b></big>" \
     --field Nome 'Marcos' \
