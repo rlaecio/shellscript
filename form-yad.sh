8bce1b619e569d3edaa02d4ba079702c8ce98192
Roque Souza <rlaecio@hotmail.com>
2019-07-04 10:01:51 +0100

    yad/zenity

---
+++ form-yad.sh
@@ -0,0 +1,13 @@
+#/bin/bash
+#Exemplo yad
+Saida=$(yad --form --imag blog-min.png --image-on-top \ 
+    --title "Terminal Root" \
+    --text "Exemplo <b>YAD</b> por <big><b>Terminal Roo</b></big>" \
+    --field Nome 'Marcos' \
+    --field Nascimento:DT 08/02/1989 \
+    --field "Há quanto tempo usa <b>Linux</b>:NUM" '1!0..20!1' \
+    --field "Sites preferidos:CB" 'Terminal Root!BR'Linux!Dicas-l!Viva o Linux!Outros' \
+    --field "Vou passar a usar o yad:CHK" TRUE \
+    --field "Vou ontinuar usando zenity:CHK")
+
+echo $Saida
